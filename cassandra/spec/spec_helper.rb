ab1c2e2ae38e9f1c544910b747a56dc214df55bd
brainopia <brainopia@evilmartians.com>
2013-11-24 17:54:05 +0400

    Workaround for local docker

--- cassandra/spec/spec_helper.rb
+++ cassandra/spec/spec_helper.rb
@@ -8,9 +8,14 @@ def Flow.default
   end
 end
 
+class Flow::Cassandra::Router
+  def local_queues
+    all_queues
+  end
+end
+
 Cassandra::Mapper.schema = { keyspaces: [:flow] }
 Cassandra::Mapper.env    = :test
-# TODO: rename to :force_migrate_when_conflict!
 Cassandra::Mapper.force_migrate
 
 RSpec.configure do |config|
