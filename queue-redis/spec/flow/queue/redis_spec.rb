46b5814e7afd2eede3ad0fc42100f377d5527cf2
brainopia <brainopia@evilmartians.com>
2013-12-02 13:32:05 +0400

    Update queue-redis specs

--- queue-redis/spec/flow/queue/redis_spec.rb
+++ queue-redis/spec/flow/queue/redis_spec.rb
@@ -8,27 +8,27 @@ describe Flow::Queue::Redis do
 
   before do
     queue.clear
-    stub_const 'Flow::Queue::ACTIONS', {}
+    stub_const 'Flow::Queue::ACTIONS_BY_NAME', {}
   end
 
   it 'should push messages' do
     Flow
-      .queue_route(queue_name)
+      .queue_transport(queue_name)
       .trigger :insert, foo: :bar
 
     queue.pull.should == {
-      action: 'queue_route',
+      action: 'queue_transport',
       type:   :insert,
       data:   { foo: :bar }
     }
   end
 
   it 'should propagate tasks futher' do
-    count.should_receive(:after_route).once
+    count.should_receive(:after_queue).once
 
     Flow
-      .queue_route(queue_name)
-      .check {|it| it.should == data; count.after_route }
+      .queue_transport(queue_name)
+      .check {|it| it.should == data; count.after_queue }
       .trigger :insert, data
 
     queue.pull_and_propagate
