38fa2f9a1e13e45c66f70cba768f173ab932a1a1
brainopia <brainopia@evilmartians.com>
2013-12-04 22:25:43 +0400

    Better integrate queue_receive action

--- queue/lib/flow/queue/receive.rb
+++ queue/lib/flow/queue/receive.rb
@@ -1,5 +1,6 @@
 class Flow::Queue::Receive < Flow::Action
   def setup_with_flow!(queue_name)
-    flow.union Flow.queue_route(queue_name)
+    parents.each {|it| it.children.delete self }
+    flow.union flow.clone_with(nil).queue_route(queue_name)
   end
 end
