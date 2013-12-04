7653e6888d9136c0e27949c96de5def3ba6242dd
brainopia <brainopia@evilmartians.com>
2013-12-04 22:29:43 +0400

    Better locations for queue_receive

--- queue/lib/flow/queue/receive.rb
+++ queue/lib/flow/queue/receive.rb
@@ -1,6 +1,8 @@
 class Flow::Queue::Receive < Flow::Action
   def setup_with_flow!(queue_name)
     parents.each {|it| it.children.delete self }
-    flow.union flow.clone_with(nil).queue_route(queue_name)
+    router = flow.clone_with(nil).queue_route(queue_name)
+    router.location = location
+    flow.union(router).tap {|it| it.action.location = location }
   end
 end
