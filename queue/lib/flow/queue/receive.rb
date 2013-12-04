bffdaaf6c2edf6755e1a4b4571ea9d4ab74a359a
brainopia <brainopia@evilmartians.com>
2013-12-04 22:31:34 +0400

    Fix location of queue_receive

--- queue/lib/flow/queue/receive.rb
+++ queue/lib/flow/queue/receive.rb
@@ -1,8 +1,11 @@
 class Flow::Queue::Receive < Flow::Action
   def setup_with_flow!(queue_name)
     parents.each {|it| it.children.delete self }
+
     router = flow.clone_with(nil).queue_route(queue_name)
-    router.location = location
-    flow.union(router).tap {|it| it.action.location = location }
+    union  = flow.union router
+
+    [router, union].each {|it| it.action.location = location }
+    union
   end
 end
